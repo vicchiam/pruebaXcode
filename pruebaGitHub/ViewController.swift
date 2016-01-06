b439ddd859c180e689c543e2b759e4e4086fd1bf
Victor Chisvert Amat <vicchiam81@gmail.com>
2016-01-06 13:33:10 +0100

    Initial Commit

---
+++ pruebaGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGitHub
+//
+//  Created by Victor Chisvert Amat on 6/1/16.
+//  Copyright © 2016 Victor Chisvert Amat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
